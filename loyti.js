f3eef603a1d6ebae9e81ec1730058fddc71b4e8d
loyti <brice@loyti.com>
2017-10-25 07:28:39 -0700

    updates active menu .html .css .js

--- loyti.js
+++ loyti.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $(function () {
       $('#myTab a:last').tab('show')
     })
-    
+
     $(".list-group a").on('click', function(event) {  
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
